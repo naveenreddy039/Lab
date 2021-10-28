844328ced94cdc7c2450d727380e43910b266207
naveen <naveenreddy.k07@gmail.com>
2021-10-28 11:58:12 +0530

    good afternoon

---
+++ Calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo
+{
+    public class Calculator
+    {
+        int totalValue;
+        public int isSum(int num1, int num2)
+        {
+            return totalValue = num1 + num2;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator objCalculator = new Calculator();
+            Console.WriteLine("total values"+objCalculator.isSum(30,20));
+            Console.ReadKey();
+        }
+    }
+}
